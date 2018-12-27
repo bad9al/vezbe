cf06bb6ba984969817460514716b19e8c03e6dc2
bad9al <46194445+bad9al@users.noreply.github.com>
2018-12-27 14:36:03 +0100

    Update V13.java

--- V13.java
+++ V13.java
@@ -1,20 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package v13;
 
-/**
- *
- * @author Nina
- */
+
 public class V13 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+        
         System.out.println("Hello World");
     }
     
